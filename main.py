26d7058c677fdeca2d35d449df23bc7e9bf89314
s-cara <110237066+s-cara@users.noreply.github.com>
2022-12-11 17:27:23 +0000

    Update main.py
    Added settings, changed handling, cleaned up code

--- main.py
+++ main.py
@@ -1,37 +1,75 @@
 from turtle import Screen, Turtle, done, bgcolor
-from vector import Vector2, Vector3
-from bezier import getPoint
+from vector import Vector2
+from bezier import getFunction
 
-Screen().setup(1000, 1000)
-bgcolor('black')
+size = 1200
+increment = 50
+
+doDots = False
+doText = False
+showPoints = True
+
+backgroundColor = 'black'
+lineColor = 'white'
+pointColor = 'red'
+textColor = 'white'
+
+Screen().setup(size, size)
+bgcolor(backgroundColor)
 
 t = Turtle()
 t.hideturtle()
 t.speed(0)
-t.color('white')
+t.pensize(2)
+    
+half = (size / 2) - 50
 
-def main():
-    v1 = Vector2(-450, -450)
-    v2 = Vector2(-450, 450)
-    v3 = Vector2(450, -450)
-    v4 = Vector2(450, 0)
+totalDiagrams = 0
+def draw(points):
+    global totalDiagrams
+    totalDiagrams = totalDiagrams + 1
 
-    points = [v1, v2, v3, v4]
-
-    for idx, point in enumerate(points):
-        t.penup()
-        t.setpos(point.x, point.y)
-        t.dot(5)
-        #t.write(idx + 1)
-
+    t.color(lineColor)
     t.penup()
-    t.setpos(v1.x, v1.y)
-    t.pendown()
+    t.setpos(points[0].x, points[0].y)
+    
+    if not doDots:
+        t.pendown()
 
-    for i in range(101):
-        target = getPoint(points, i / 100)
+    f = getFunction(points)
+
+    for i in range(increment + 1):
+        target = f(points, i / increment, change=0)
 
         t.goto(target.x, target.y)
+
+        if doDots:
+            t.dot(3, lineColor)
+
+    t.color(textColor)
+
+    if showPoints:
+        for idx, point in enumerate(points):
+            t.penup()
+            t.setpos(point.x, point.y)
+            t.dot(5, pointColor)
+        
+            if doText:
+                t.write(f'{totalDiagrams}:{idx+1}', align='center', font=('Arial', 14, 'bold'))
+
+def main():
+    draw([
+        Vector2(0, half),
+        Vector2(-half, 0),
+        Vector2(-half, -half)
+    ])
+
+    draw([
+        Vector2(half, half),
+        Vector2(-(half / 2), 0),
+        Vector2(0, -half),
+        Vector2(half, -half)
+    ])
 
     done()
 
